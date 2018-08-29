a1d2857415617c3b4f2981d5f269c913c6e0a2f9
wangliang01 <1392830517@qq.com>
2018-08-29 16:02:51 +0800

    jsBridge.js

--- 其他/test.js
+++ 其他/test.js
@@ -1,20 +1,37 @@
-var qcArray = function (arr) {
-  let res = []
-  console.log('qcArray')
-  for (let i = 0; i < arr.length; i++) {
-    let flag = true
-    for (let j = 0; j < res.length; j++) {
-      if (arr[i] === res[j]) {
-        flag = false
-        break;
-      }
-    }
-    if (flag) {
-      res.push(arr[i])
-    }
+function Foo() {
+  // 私有方法
+  getName = function () {
+    console.log(1)
   }
-  console.log(res)
-  return res
- }
+  return this
+}
 
- qcArray([1, 4, 3, 2, 2, 3, 4, 5])
+
+// 静态方法，不需要new， 可以直接调用
+Foo.getName = function () {
+  console.log(2)
+}
+
+// 公有方法，需要创建实例，才可以调用
+Foo.prototype.getName = function () {
+  console.log(3)
+}
+
+var getName = function () {
+  console.log(4)
+}
+
+function getName() {
+  console.log(5)
+}
+
+Foo.getName()
+
+getName()
+
+Foo().getName()
+
+getName()
+
+new Foo().getName()
+
