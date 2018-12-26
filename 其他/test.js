632f420d012df1788d6f9db08cad4716db262b4f
王亮 <wangliang320@crnet.com.hk>
2018-12-26 10:08:42 +0800

    随便写写

--- 其他/test.js
+++ 其他/test.js
@@ -1,38 +1,34 @@
-var obj = new Proxy({}, {
-  get: function (target, key, receiver) {
-    console.log(`getting ${key}`)
-    return Reflect.get(target, key, receiver)
-  },
-  set: function (target, key, value, receiver) {
-    console.log(`setting ${key}`)
-    return Reflect.set(target, key, value, receiver)
-   }
-})
-
-var proxy = new Proxy(target, handler)
-
-var proxy = new Proxy({}, {
-  get: function (target, property) {
-    return 35
+class Event {
+  constructor() {
+    this.eventList = []
   }
-})
+  on(eventName, handler) {
+    if (!this.eventList[eventName]) {
+      this.eventList[eventName] = []
+    }
+    this.eventList[eventName].push(handler)
+    return this
+  }
+  off(eventName, handler) {
+    this.eventList[eventName] = this.eventList[eventName].filter(item => item != handler)
+    return this
+  }
+  emit(eventName, data) {
+    this.eventList[eventName].forEach(item => item.call(this, data))
+    return this
+  }
+}
 
-// proxy接受两个参数。第一个参数是所要代理的目标对象（上例是一个空对象），即如果没有proxy介入，操作原来要访问的就是这个对象；第二个参数就是一个配置对象，对于每一个被代理的操作，需要提供一个对应的处理函数，该函数将拦截对应的操作。
+var event = new Event()
 
-// 同一个拦截函数，可以设置拦截多个操作
+const handler = function (data) {
+  console.log('我被点击啦', data)
+}
 
-// Proxy支持的拦截操作一共有13种
-get(target, propKey, receiver)
-set(target, propKey, value, receiver)
-has(target, propKey)
-deleteProperty(target, propKey)
-ownKeys(target)
-getOwnPropertyDescriptor(target, propKey)
-defineProperty(target, propKey, propDesc)
-preventExtensions(target)
-getPrototypeOf(target)
-isExtensible(target)
-setPrototypeOf(target, proto)
-apply(target, object, args)
-construct(target, args)
-proxy.time
+event.on('click', handler)
+
+event.emit('click', 'haha')
+
+event.off('click', handler)
+
+event.emit('click', '哈哈')
